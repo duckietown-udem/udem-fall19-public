aafb5a9d142a44ab83073af9247f5c677644bb2b
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-07-31 22:19:01 -0400

    adding 03-imitation-learning

--- helpers.py
+++ helpers.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import Tuple
 
 import numpy as np
+import gym
+from gym import spaces
 
 from aido_schemas import EpisodeStart, protocol_agent_duckiebot1, PWMCommands, Duckiebot1Commands, LEDSCommands, RGB, \
     wrap_direct, Context, Duckiebot1Observations, JPGImage, Context
@@ -97,3 +99,103 @@ def view_results_ipython(env, results_path='./gym_results'):
     return HTML(data='''
         <video width="360" height="auto" alt="test" controls><source src="data:video/mp4;base64,{0}" type="video/mp4" /></video>'''
     .format(encoded.decode('ascii')))
+
+class SteeringToWheelVelWrapper():
+    """
+    Converts policy that was trained with [velocity|heading] actions to
+    [wheelvel_left|wheelvel_right] to comply with AIDO evaluation format
+    """
+
+    def __init__(self, gain=1.0, trim=0.0, radius=0.0318, k=27.0, limit=1.0, wheel_dist=0.102):
+        # Should be adjusted so that the effective speed of the robot is 0.2 m/s
+        self.gain = gain
+
+        # Directional trim adjustment
+        self.trim = trim
+
+        # Wheel radius
+        self.radius = radius
+
+        # Motor constant
+        self.k = k
+
+        # Wheel velocity limit
+        self.limit = limit
+
+        # Distance between wheels
+        self.wheel_dist = wheel_dist
+
+    def convert(self, action):
+        vel, angle = action
+
+        # Distance between the wheels
+        baseline = self.wheel_dist
+
+        # assuming same motor constants k for both motors
+        k_r = self.k
+        k_l = self.k
+
+        # adjusting k by gain and trim
+        k_r_inv = (self.gain + self.trim) / k_r
+        k_l_inv = (self.gain - self.trim) / k_l
+
+        omega_r = (vel + 0.5 * angle * baseline) / self.radius
+        omega_l = (vel - 0.5 * angle * baseline) / self.radius
+
+        # conversion from motor rotation rate to duty cycle
+        u_r = omega_r * k_r_inv
+        u_l = omega_l * k_l_inv
+
+        # limiting output to limit, which is 1.0 for the duckiebot
+        u_r_limited = max(min(u_r, self.limit), -self.limit)
+        u_l_limited = max(min(u_l, self.limit), -self.limit)
+
+        vels = np.array([u_l_limited, u_r_limited])
+        return vels
+
+class ResizeWrapper(gym.ObservationWrapper):
+    # TODO: Fix with internet!!
+    def __init__(self, env=None, shape=(120, 160, 3)):
+        super(ResizeWrapper, self).__init__(env)
+        self.observation_space.shape = shape
+        self.observation_space = spaces.Box(
+            self.observation_space.low[0, 0, 0],
+            self.observation_space.high[0, 0, 0],
+            shape,
+            dtype=self.observation_space.dtype)
+        self.shape = shape
+
+    def observation(self, observation):
+        from scipy.misc import imresize
+        return imresize(observation, self.shape)
+
+class NormalizeWrapper(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(NormalizeWrapper, self).__init__(env)
+        self.obs_lo = self.observation_space.low[0, 0, 0]
+        self.obs_hi = self.observation_space.high[0, 0, 0]
+        obs_shape = self.observation_space.shape
+        self.observation_space = spaces.Box(0.0, 1.0, obs_shape, dtype=np.float32)
+
+    def observation(self, obs):
+        if self.obs_lo == 0.0 and self.obs_hi == 1.0:
+            return obs
+        else:
+            return (obs - self.obs_lo) / (self.obs_hi - self.obs_lo)
+
+
+class ImgWrapper(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ImgWrapper, self).__init__(env)
+        obs_shape = self.observation_space.shape
+        self.observation_space = spaces.Box(
+            self.observation_space.low[0, 0, 0],
+            self.observation_space.high[0, 0, 0],
+            [obs_shape[2], obs_shape[0], obs_shape[1]],
+            dtype=self.observation_space.dtype)
+
+    def observation(self, observation):
+        return observation.transpose(2, 0, 1)
+
+def force_done(env):
+    env.done = True
