592a1c6939f5e09401c31f54ac3998658947f808
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-08-03 13:54:01 -0400

    Docker and ros (#1)
    * working notebook
    
    * docker compose initial
    
    * updating, notebook connections work!

--- initial.sh
+++ initial.sh
@@ -5,6 +5,13 @@ else
   git clone https://github.com/montrealrobotics/gym-duckietown simulation
 fi 
 
+# Clone the duckietown/Software repository
+if [ -d "software" ]; then
+  echo "software/ directory exists, skipping clone of duckietown/Software..";
+else
+  git clone https://github.com/duckietown/Software software
+fi 
+
 # pip install requirements for this class
 pip install -r requirements.txt
 
