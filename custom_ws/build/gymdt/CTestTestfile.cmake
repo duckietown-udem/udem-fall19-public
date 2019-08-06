44731bbef61056a5b490468ae7b84bda71bbb9fe
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-08-06 18:22:36 -0400

    Dt msg (#6)
    * adding
    
    * adding finished ros pipeline
    
    * force push src
    
    * adding fully functioning ros baseline
    
    * adding
    
    * rm dtmsg

---
+++ custom_ws/build/gymdt/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /duckietown/custom_ws/src/gymdt
+# Build directory: /duckietown/custom_ws/build/gymdt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ custom_ws/src/gymdt/scripts/gymdt_node.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import sys
+if '/duckietown/' not in sys.path:
+    sys.path.append('/duckietown/')
+
+from utils.ros_helpers import launch_env
+
+import rospy
+from sensor_msgs.msg import CompressedImage, CameraInfo
+from duckietown_msgs.msg import Twist2DStamped, WheelsCmdStamped
+import numpy as np
+import os
+import cv2
+
+
+class ROSAgent(object):
+    def __init__(self):
+        # Get the vehicle name, which comes in as HOSTNAME
+        # self.vehicle = os.getenv('HOSTNAME')
+        self.vehicle = 'baseline'
+
+        self.ik_action_sub = rospy.Subscriber('/{}/wheels_driver_node/wheels_cmd'.format(
+            self.vehicle), WheelsCmdStamped, self._ik_action_cb)
+        # Place holder for the action, which will be read by the agent in solution.py
+        self.action = np.array([0, 0])
+        self.updated = True
+
+        # Publishes onto the corrected image topic 
+        # since image out of simulator is currently rectified
+        self.cam_pub = rospy.Publisher('/{}/corrected_image/compressed'.format(
+            self.vehicle), CompressedImage, queue_size=10)
+        
+        # Publisher for camera info - needed for the ground_projection
+        self.cam_info_pub = rospy.Publisher('/{}/camera_info_topic'.format(
+            self.vehicle), CameraInfo, queue_size=1)
+
+        # Initializes the node
+        rospy.init_node('GymDuckietown')
+
+    def _ik_action_cb(self, msg):
+        """
+        Callback to listen to last outputted action from inverse_kinematics node
+        Stores it and sustains same action until new message published on topic
+        """
+        vl = msg.vel_left
+        vr = msg.vel_right
+        self.action = np.array([vl, vr])
+        self.updated = True
+    
+    def _publish_info(self):
+        """
+        Publishes a default CameraInfo
+        """
+
+        self.cam_info_pub.publish(CameraInfo())      
+
+    def publish_img(self, obs):
+        """
+        Publishes the image to the compressed_image topic, which triggers the lane following loop
+        """
+        img_msg = CompressedImage()
+
+        time = rospy.get_rostime()
+        img_msg.header.stamp.secs = time.secs
+        img_msg.header.stamp.nsecs = time.nsecs
+
+        img_msg.format = "jpeg"
+        contig = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_BGR2RGB)
+        img_msg.data = np.array(cv2.imencode('.jpg', contig)[1]).tostring()
+
+        self.cam_pub.publish(img_msg)
+        self._publish_info()
+
+if __name__ == '__main__':
+    rosagent = ROSAgent()
+    env = launch_env()
+    obs = env.reset()
+    r = rospy.Rate(15)  
+    rosagent.publish_img(obs)
+
+    while not rospy.is_shutdown():
+        action = rosagent.action
+        obs, reward, done, _ = env.step(action)
+
+        if done:
+            obs = env.reset()
+
+        rosagent.publish_img(obs)
+        r.sleep()
+
