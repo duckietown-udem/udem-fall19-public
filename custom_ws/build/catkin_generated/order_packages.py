f2422276225f9dabd0baea5b731ba2ece2ec827a
Liam Paull <paulll@iro.umontreal.ca>
2019-09-04 11:26:16 -0400

    Baseline fix (#10)
    * fixing submodule stuff and some other small things
    
    * Update .gitmodules
    
    * removing build artifacts from git
    
    * small
    
    * small

--- custom_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/duckietown/custom_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/duckietown/software/catkin_ws/devel;/opt/ros/melodic".split(';') if "/duckietown/software/catkin_ws/devel;/opt/ros/melodic" != "" else []
