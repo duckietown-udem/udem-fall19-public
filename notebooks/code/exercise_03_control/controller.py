d331dc50b755d976013c22606884a6ad583fa581
Liam Paull <paulll@iro.umontreal.ca>
2019-09-23 22:47:16 -0400

    moving in SW ex. 3

---
+++ notebooks/code/exercise_03_control/controller.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+
+class Controller():
+    def __init__(self):
+        self.gain = 2.0
+        pass
+
+    def angle_control_commands(self, dist, angle):
+        # Return the angular velocity in order to control the Duckiebot so that it follows the lane.
+        # Parameters:
+        #     dist: distance from the center of the lane. Left is negative, right is positive.
+        #     angle: angle from the lane direction, in rad. Left is negative, right is positive.
+        # Outputs:
+        #     omega: angular velocity, in rad/sec. Right is negative, left is positive.
+        
+        omega = 0. 
+        
+        #######
+        #
+        # MODIFY ANGULAR VELOCITY
+        #
+        # YOUR CODE HERE
+        #
+        #######
+        
+        return  omega
+
+    def pure_pursuit(self, env, pos, angle, follow_dist=0.25):
+        # Return the angular velocity in order to control the Duckiebot using a pure pursuit algorithm.
+        # Parameters:
+        #     env: Duckietown simulator
+        #     pos: global position of the Duckiebot
+        #     angle: global angle of the Duckiebot
+        # Outputs:
+        #     v: linear veloicy in m/s.
+        #     omega: angular velocity, in rad/sec. Right is negative, left is positive.
+        
+        
+        closest_curve_point = env.unwrapped.closest_curve_point
+        
+        # Find the curve point closest to the agent, and the tangent at that point
+        closest_point, closest_tangent = closest_curve_point(pos, angle)
+
+        iterations = 0
+        
+        lookup_distance = follow_dist
+        multipler = 0.5
+        curve_point = None
+        
+        while iterations < 10:            
+            ########
+            #
+            #TODO 1: Modify follow_point so that it is a function of closest_point, closest_tangent, and lookup_distance
+            #
+            ########
+            follow_point = closest_point
+            
+            curve_point, _ = closest_curve_point(follow_point, angle)
+
+            # If we have a valid point on the curve, stop
+            if curve_point is not None:
+                break
+
+            iterations += 1
+            lookup_distance *= multiplier
+        ########
+        #
+        #TODO 2: Modify omega
+        #
+        ########
+        omega = 0.
+        v = 0.5
+
+        return v, omega
